55ca0dbb705f76533022343c2466d55ab807f2d4
Dacal Cantos <jorge.dacal-cantos@capgemini.com>
2018-02-12 11:12:24 +0100

    Refactor all to use the DevonFw templates system.

--- build.sh
+++ build.sh
@@ -1,44 +1,65 @@
-#!/bin/bash
+#/bin/bash
 
-# Create project to store build base-images
-oc new-project oasp --display-name='OASP' --description='Open Application Standard Platform'
+RED="\033[31m"
+BLUE="\033[34m"
+NC="\033[0m"
 
-# Create base-images and add them to OASP project
-oc create -f https://raw.githubusercontent.com/oasp/s2i/master/s2i/java/s2i-oasp-java-imagestream.json --namespace=oasp
-oc create -f https://raw.githubusercontent.com/oasp/s2i/master/s2i/angular/s2i-oasp-angular-imagestream.json --namespace=oasp
+echo -e "${BLUE}
+      ____                         __            ____  _
+     |  _ \  _____   _____  _ __  / _|_      __ / ___|| |__   ___  _ __
+     | | | |/ _ \ \ / / _ \| '_ \| |_\ \ /\ / / \___ \| '_ \ / _ \| '_ \\
+     | |_| |  __/\ V / (_) | | | |  _|\ V  V /   ___) | | | | (_) | |_) |
+     |____/ \___| \_/ \___/|_| |_|_|   \_/\_/   |____/|_| |_|\___/| .__/
+                                                                  |_|
+                   _____ _                     __
+                  |  ___| | ___   ___  _ __   / _| ___  _ __
+                  | |_  | |/ _ \ / _ \| '__| | |_ / _ \| '__|
+                  |  _| | | (_) | (_) | |    |  _| (_) | |
+                  |_|   |_|\___/ \___/|_|    |_|  \___/|_|
 
-# Build base-images in OASP project
-oc start-build s2i-oasp-java --namespace=oasp
-oc start-build s2i-oasp-angular --namespace=oasp
+                   ___                   ____  _     _  __ _
+                  / _ \ _ __   ___ _ __ / ___|| |__ (_)/ _| |_
+                 | | | | '_ \ / _ \ '_ \\___ \| '_ \| | |_| __|
+                 | |_| | |_) |  __/ | | |___) | | | | |  _| |_
+                  \___/| .__/ \___|_| |_|____/|_| |_|_|_|  \__|
+                       |_|
+                                    _ _ _
+${NC}"
+
+echo "Login as admin"
+oc login
+
+oc adm policy add-cluster-role-to-user cluster-admin system
+
+# STEP 2: create the devonFW project and add the s2i base-images.
+
+## Create project to store build base-images
+oc new-project devonfw --display-name='DevonFW' --description='DevonFW'
+
+## Create base-images and add them to DevonFW project
+### this files are private, to share it, you must enter in Git with a valid user, open the file and press RAW Button to generate a valid token
+oc create -f https://raw.githubusercontent.com/oasp/s2i/master/s2i/angular/s2i-devonfw-java-imagestream.json --namespace=devonfw
+oc create -f https://raw.githubusercontent.com/oasp/s2i/master/s2i/angular/s2i-devonfw-angular-imagestream.json --namespace=devonfw
+
+## Build base-images in DevonFW project
+oc start-build s2i-devonfw-java --namespace=devonfw
+oc start-build s2i-devonfw-angular --namespace=devonfw
 
 sleep 30
-ret=`oc status -v -n oasp | grep 'running for'`
+ret=`oc status -v -n devonfw | grep 'running for'`
 while [[ !  -z  $ret  ]]; do
     echo "Waiting for build to complete..."
-    ret=`oc status -v -n oasp | grep 'running for'`
+    ret=`oc status -v -n devonfw | grep 'running for'`
     sleep 30
 done
 
-# Setup the OASP project as "image-puller" to be used in other projects in the same cluster
-oc policy add-role-to-group system:image-puller system:authenticated --namespace=oasp
+# STEP 3: Setup the DevonFW project as "image-puller" to be used in other projects in the same cluster
+oc policy add-role-to-group system:image-puller system:authenticated --namespace=devonfw
 
-# Create project to host My Thai Star applications built from OASP's base-images
-oc new-project mythaistar --display-name='My Thai Star' --description='My Thai Star reference application for OASP'
+# STEP 4: Create DevonFW templates into openshift
+### this files are private, to share it, you must enter in Git with a valid user, open the file and press RAW Button to generate a valid token
+oc create -f https://raw.githubusercontent.com/oasp/s2i/master/templates/devonfw/devonfw-java-template.json --namespace=openshift
+oc create -f https://raw.githubusercontent.com/oasp/s2i/master/templates/devonfw/devonfw-angular-template.json --namespace=openshift
 
-# Create templates for both MTS's Angular client and Java server
-oc create -f https://raw.githubusercontent.com/oasp/s2i/master/templates/mythaistar/oasp-mythaistar-java-template.json --namespace=mythaistar
-oc create -f https://raw.githubusercontent.com/oasp/s2i/master/templates/mythaistar/oasp-mythaistar-angular-template.json --namespace=mythaistar
-
-# Create Java application out of the Java template 
-oc new-app --template=oasp-mythaistar-java-sample --namespace=mythaistar
-# Build Java application
-oc start-build mythaistar-java --namespace=mythaistar
-
-# Create Angular application out of the Angular template
-oc new-app --template=oasp-mythaistar-angular-sample --namespace=mythaistar
-
-sleep 10
-# Setup Environment Variable pointing to Java application's URL
-oc set env bc/mythaistar-angular REST_ENDPOINT_URL=http://`oc get routes mythaistar-java --no-headers=true --namespace=mythaistar | sed -e's/  */ /g' | cut -d" " -f 2` --namespace=mythaistar
-# Build Angular application (sets the E.V. in the Angular code)
-oc start-build mythaistar-angular --namespace=mythaistar
+echo -e "\nCluster Initial Setup finish.\n"
+read -n1 -r -p "Press any key to close..." key
